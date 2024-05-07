bdb61d0a9c73f3af2e9fe03727d00043a3a73024
Ahmad Banihani <banihania91@gmail.com>
2024-05-07 16:11:24 +0300

    x

---
+++ Task/models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(15, ErrorMessage = "First name cannot exceed 15 characters.")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(15, ErrorMessage = "Last name cannot exceed 15 characters.")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string email { get; set; }
+
+        [Phone(ErrorMessage = "Phone number must only contain numbers")]
+        [StringLength(10,MinimumLength =10, ErrorMessage = "Phone number must be exactly 10 digits long.")]
+        public string phoneNumber { get; set; }
+      
+    }
+
+}
---
+++ Task/models/contactList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace models
+{
+    public  class contactList
+    {
+        public static List<Contact> Contacts { get; set; } = new List<Contact> {
+           new Contact {Id=0,firstName="ahmad",lastName="mahmoud",email="ahmad@email.com",phoneNumber="0123456789"},
+           new Contact {Id=1,firstName="osama",lastName="mahmoud",email="osama@email.com",phoneNumber="0792119858"},
+            };
+    }
+}
